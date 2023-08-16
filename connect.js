b7dc64a58d9d7b5c15043d1f6c1a1de12581b3f2
Akshay <patelakshay6702@gmail.com>
2023-08-16 13:46:28 +0530

    updated mongodb connection

--- connect.js
+++ connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectoToDB = async() => {
-    mongoose.connect("mongodb://127.0.0.1:27017/api", {
+    mongoose.connect("mongodb+srv://akshay:akshay@cluster0.chjo92h.mongodb.net/?retryWrites=true&w=majority", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
